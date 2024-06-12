1bb1d6e400a9b51b7bef45621ae9ac7d77fd503b
iqbal-dev <pkmdiqbal@gmail.com>
2024-06-12 08:08:14 +0600

    [updtae] validate request and cath async function

--- src/app/middleware/validationRequest.ts
+++ src/app/middleware/validationRequest.ts
@@ -1,20 +1,13 @@
-import { NextFunction, Request, Response } from 'express';
 import { AnyZodObject } from 'zod';
+import catchAsync from '../utils/catchAsync';
 
 const validateRequest = (schema: AnyZodObject) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      // validation check
-      //if everything allright next() ->
-      await schema.parseAsync({
-        body: req.body,
-      });
-
-      next();
-    } catch (err) {
-      next(err);
-    }
-  };
+  return catchAsync(async (req, res, next) => {
+    await schema.parseAsync({
+      body: req.body,
+    });
+    next();
+  });
 };
 
 export default validateRequest;
